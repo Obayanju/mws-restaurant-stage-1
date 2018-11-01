67474071a761dabcb13abaeb0e4baab727f7629a
Dami <obayanjud@gmail.com>
2018-10-31 22:11:19 -0600

    Add Aria labels

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -189,8 +189,9 @@ createReviewHTML = review => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant = self.restaurant) => {
-  const breadcrumb = document.getElementById("breadcrumb");
+  const breadcrumb = document.querySelector("#breadcrumb ol");
   const li = document.createElement("li");
+  li.setAttribute("aria-current", "page");
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
 };
